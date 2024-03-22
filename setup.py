63b4b338bc03ea69234bbefa25b17ba0c3c2ca99
christinachen94 <christina.chen94@gmail.com>
2024-03-22 12:49:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from distutils.core import setup
+import setuptools
 
-setup(name='cuda_gridsample',
-      py_modules=['cuda_gridsample'],
-      )
+setuptools.setup(
+      name = 'cuda_gridsample',
+      packages = setuptools.find_packages(),
+      url = 'https://github.com/christinachen94/cuda-gridsample-grad2')
