506379a52a1a0892cce1675f620967bc871bc24c
christinachen94 <christina.chen94@gmail.com>
2024-04-02 14:56:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-import setuptools
+from setuptools import setup, Extension
 
-setuptools.setup(
-      name = 'cuda_gridsample',
+setup(name = 'cuda_gridsample',
       packages = setuptools.find_packages(),
       url = 'https://github.com/christinachen94/cuda-gridsample-grad2',
-      ext_modules = [Extension(name='extensions', 
+      ext_modules = [Extension(name='cuda_gridsample.extension', 
                                sources=['backend/cuda_gridsample.cpp', 
                                         'backend/cuda_gridsample.cu']
                               )
