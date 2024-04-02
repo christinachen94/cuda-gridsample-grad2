a2a877690645371b98430c7ce29911ad59868b72
Christina  Chen <christinachen@Christinas-MacBook-Pro.local>
2024-04-02 14:25:33 -0400

    same changes

--- setup.py
+++ setup.py
@@ -3,4 +3,10 @@ import setuptools
 setuptools.setup(
       name = 'cuda_gridsample',
       packages = setuptools.find_packages(),
-      url = 'https://github.com/christinachen94/cuda-gridsample-grad2')
+      url = 'https://github.com/christinachen94/cuda-gridsample-grad2',
+      ext_modules = [Extension(name='extensions', 
+                               sources=['backend/cuda_gridsample.cpp', 
+                                        'backend/cuda_gridsample.cu']
+                              )
+                    ]
+)
